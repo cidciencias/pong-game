5535afd2bf1782e4d65614c07c6f2a55bbef68b7
Luis Cid <alexcid2002@gmail.com>
2024-06-05 12:06:01 +0100

    pong-game V1.5
    Ball out of bounds logic implemented. If the ball gets out of bounds, it resets its position to the midle of the screen

--- main.py
+++ main.py
@@ -36,5 +36,8 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect paddle misses
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
